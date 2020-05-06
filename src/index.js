b4fd0ab77e0ff12e035cf6b1c58890725d2d2dbf
Ravina Deogadkar <ravina.deogadkar@cctech.co.in>
2020-05-06 19:42:52 +0530

    Refactor: created component folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Converter from "./Converter";
+import Converter from "./Component/Converter";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
